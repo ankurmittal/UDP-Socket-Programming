c5ab411590f7117f9cfd7c9092a8344304a14ae8
Ankur Mittal <ankurmit2006@gmail.com>
2014-10-30 20:10:36 -0400

    Forgot to commit sendmessages.h

---
+++ lib/sendmessages.h
@@ -0,0 +1,12 @@
+
+#ifndef	__sendmessages_h
+#define	__sendmessages_h
+
+typedef int (*callback)(int numberofsegments);
+void init_sender(int window, int f); 
+void setsecondaryfd(int s);
+void setprimaryfd(int s);
+int isswfull();
+void insertmsg(void *outbuff, int outlen);
+int dg_send(callback c);
+#endif	
